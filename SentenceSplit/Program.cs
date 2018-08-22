ed066ebb1dd742cb9a7723b1ae0e01d5c8e26146
Suyash1234567 <suyashmehrotra85@gmail.com>
2018-08-22 19:10:55 +0530

    First commit

---
+++ SentenceSplit/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentenceSplit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Sentence that is to be split");
+            string sentence = Console.ReadLine();
+            String[] words = sentence.Split(' ');
+            foreach(String word in words)
+            {
+                Console.WriteLine(word);
+            }
+            Console.ReadLine();
+        }
+    }
+}
